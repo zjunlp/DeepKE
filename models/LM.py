de97e387d96a7d95ecd26be1982b739da3d36f9e
leo <yuwl798180@163.com>
2019-12-03 22:42:51 +0800

    update lm

---
+++ models/LM.py
@@ -0,0 +1,24 @@
+from torch import nn
+from . import BasicModule
+from module import RNN
+from transformers import BertModel
+from utils import seq_len_to_mask
+
+
+class LM(BasicModule):
+    def __init__(self, cfg):
+        super(LM, self).__init__()
+        self.bert = BertModel.from_pretrained(cfg.lm_file, num_hidden_layers=cfg.num_hidden_layers)
+        self.bilstm = RNN(cfg)
+        self.fc = nn.Linear(cfg.hidden_size, cfg.num_relations)
+        self.dropout = nn.Dropout(cfg.dropout)
+
+    def forward(self, x):
+        word, lens = x['word'], x['lens']
+        mask = seq_len_to_mask(lens, mask_pos_to_true=False)
+        last_hidden_state, pooler_output = self.bert(word, attention_mask=mask)
+        out, out_pool = self.bilstm(last_hidden_state, lens)
+        out_pool = self.dropout(out_pool)
+        output = self.fc(out_pool)
+
+        return output
