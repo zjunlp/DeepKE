f4b3d660fa9a0ab89ca0d24fd00c42860d589da7
tlk-dsg <467460833@qq.com>
2021-09-16 16:17:57 +0800

    test

--- example/ae/standard/predict.py
+++ example/ae/standard/predict.py
@@ -4,8 +4,8 @@ import torch
 import logging
 import hydra
 from hydra import utils
-from deepke.ae_re_tools import Serializer
-from deepke.ae_re_tools import _serialize_sentence, _convert_tokens_into_index, _add_pos_seq, _handle_attribute_data
+from deepke.ae_st_tools import Serializer
+from deepke.ae_st_tools import _serialize_sentence, _convert_tokens_into_index, _add_pos_seq, _handle_attribute_data
 import matplotlib.pyplot as plt
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 from deepke.ae_st_utils import load_pkl, load_csv
