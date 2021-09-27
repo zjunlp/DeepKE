eb31ad68461cf1fa5834acb5dd4f2db1e21e5c15
tlk-dsg <467460833@qq.com>
2021-09-27 20:22:59 +0800

    test

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='deepke',  # 打包后的包文件名
-    version='0.2.25',    #版本号
+    version='0.2.27',    #版本号
     keywords=["pip", "RE","NER","AE"],    # 关键字
     description='DeepKE 是基于 Pytorch 的深度学习中文关系抽取处理套件。',  # 说明
     long_description="client",  #详细说明
@@ -13,6 +13,19 @@ setup(
     platforms="any",
     package_dir={"": "src"},
     packages=find_packages("src"),
+    install_requires=[
+        'torch>=1.2',
+        'hydra-core==1.0.6',
+        'tensorboard>=2.0',
+        'matplotlib>=3.1',
+        'transformers>=2.0',
+        'jieba>=0.39',
+        'scikit-learn>=0.22',
+        'pytorch-transformers>=1.2.0',
+        'seqeval>=0.0.5',
+        'tqdm>=4.31.1',
+        'nltk>=3.4.5'
+    ], 
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
