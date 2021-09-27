14ad7c9571da755a7c99cb73eef8043cc5a3f6a5
lilei <46843105+Flow3rDown@users.noreply.github.com>
2021-09-27 09:06:04 -0500

    ner-few-shot

---
+++ example/ner/few-shot/README.md
@@ -0,0 +1,56 @@
+## 快速上手
+
+### 环境依赖
+
+> python == 3.8
+
+- torch == 1.7
+- tensorboardX ==2.4
+- transformers == 3.4.0
+- deepke 
+
+### 克隆代码
+```
+git clone git@github.com:zjunlp/DeepKE.git
+```
+### 使用pip安装
+
+首先创建python虚拟环境，再进入虚拟环境
+
+- 安装依赖: ```pip install -r requirements.txt```
+
+### 使用数据进行训练预测
+
+- 存放数据：在 `data` 文件夹下存放训练数据。包含conll2003，mit-movie，mit-restaurant和atis等数据集。
+
+- conll2003包含以下数据：
+
+  - `train.txt`：存放训练数据集
+
+  - `dev.txt`：存放验证数据集
+
+  - `test.txt`：存放测试数据集
+
+  - `indomain-train.txt`：存放indomain数据集
+
+- mit-movie, mit-restaurant和atis包含以下数据：
+
+  - `k-shot-train.txt`：k=[10, 20, 50, 100, 200, 500]，存放训练数据集
+
+  - `test.txt`：存放测试数据集
+
+
+- 开始训练：模型加载和保存位置以及配置可以在shell脚本中修改
+  
+  - 训练conll2003：` python run.py ` (训练所用到参数都在conf文件夹中，修改即可)
+
+  - 进行few-shot训练：` python run.py +train=few_shot ` (若要加载模型，修改few_shot.yaml中的load_path)
+
+
+- 每次训练的日志保存在 `logs` 文件夹内，模型结果保存目录可以自定义。
+
+- 进行预测： ` python predict.py `
+
+
+## 模型内容
+LightNER
---
+++ example/ner/few-shot/run.py
@@ -0,0 +1,109 @@
+import os
+
+import hydra
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]='1'
+import logging
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
+from hydra import utils
+from torch.utils.data import DataLoader
+from deepke.name_entity_re.few_shot.models.model import PromptBartModel, PromptGeneratorModel
+from deepke.name_entity_re.few_shot.module.datasets import ConllNERProcessor, ConllNERDataset
+from deepke.name_entity_re.few_shot.module.train import Trainer
+from deepke.name_entity_re.few_shot.module.metrics import Seq2SeqSpanMetric
+from deepke.name_entity_re.few_shot.utils.utils import get_loss, set_seed
+from deepke.name_entity_re.few_shot.module.mapping_type import mit_movie_mapping, mit_restaurant_mapping, atis_mapping
+
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+from tensorboardX import SummaryWriter
+writer = SummaryWriter(log_dir='logs')
+
+logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt = '%m/%d/%Y %H:%M:%S',
+                    level = logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+DATASET_CLASS = {
+    'conll2003': ConllNERDataset,
+    'mit-movie': ConllNERDataset,
+    'mit-restaurant': ConllNERDataset,
+    'atis': ConllNERDataset
+}
+
+DATA_PROCESS = {
+    'conll2003': ConllNERProcessor,
+    'mit-movie': ConllNERProcessor,
+    'mit-restaurant': ConllNERProcessor,
+    'atis': ConllNERProcessor
+}
+
+DATA_PATH = {
+    'conll2003': {'train': 'data/conll2003/train.txt',
+                  'dev': 'data/conll2003/dev.txt',
+                  'test': 'data/conll2003/test.txt'},
+    'mit-movie': {'train': 'data/mit-movie/20-shot-train.txt',
+                  'dev': 'data/mit-movie/test.txt'},
+    'mit-restaurant': {'train': 'data/mit-restaurant/10-shot-train.txt',
+                  'dev': 'data/mit-restaurant/test.txt'},
+    'atis': {'train': 'data/atis/20-shot-train.txt',
+                  'dev': 'data/atis/test.txt'}
+}
+
+MAPPING = {
+    'conll2003': {'loc': '<<location>>',
+                'per': '<<person>>',
+                'org': '<<organization>>',
+                'misc': '<<others>>'},
+    'mit-movie': mit_movie_mapping,
+    'mit-restaurant': mit_restaurant_mapping,
+    'atis': atis_mapping
+}
+
+@hydra.main(config_path="conf/config.yaml")
+def main(cfg):
+    cwd = utils.get_original_cwd()
+    cfg.cwd = cwd
+    print(cfg)
+    
+    data_path = DATA_PATH[cfg.dataset_name]
+    for mode, path in data_path.items():
+        data_path[mode] = os.path.join(cfg.cwd, path)
+    dataset_class, data_process = DATASET_CLASS[cfg.dataset_name], DATA_PROCESS[cfg.dataset_name]
+    mapping = MAPPING[cfg.dataset_name]
+
+    set_seed(cfg.seed) # set seed, default is 1
+    if cfg.save_path is not None:  # make save_path dir
+        cfg.save_path = os.path.join(cfg.save_path, cfg.dataset_name+"_"+str(cfg.batch_size)+"_"+str(cfg.learning_rate)+cfg.notes)
+        if not os.path.exists(cfg.save_path):
+            os.makedirs(cfg.save_path, exist_ok=True)
+    
+    process = data_process(data_path=data_path, mapping=mapping, bart_name=cfg.bart_name, learn_weights=cfg.learn_weights)
+    train_dataset = dataset_class(data_processor=process, mode='train')
+    train_dataloader = DataLoader(train_dataset, collate_fn=train_dataset.collate_fn, batch_size=cfg.batch_size, num_workers=4)
+    
+    dev_dataset = dataset_class(data_processor=process, mode='dev')
+    dev_dataloader = DataLoader(dev_dataset, collate_fn=dev_dataset.collate_fn, batch_size=cfg.batch_size, num_workers=4)
+
+    label_ids = list(process.mapping2id.values())
+
+    prompt_model = PromptBartModel(tokenizer=process.tokenizer, label_ids=label_ids, args=cfg)
+    model = PromptGeneratorModel(prompt_model=prompt_model, bos_token_id=0,
+                                eos_token_id=1,
+                                max_length=cfg.tgt_max_len, max_len_a=cfg.src_seq_ratio,num_beams=cfg.num_beams, do_sample=False,
+                                repetition_penalty=1, length_penalty=cfg.length_penalty, pad_token_id=1,
+                                restricter=None)
+    metrics = Seq2SeqSpanMetric(eos_token_id=1, num_labels=len(label_ids), target_type='word')
+    loss = get_loss
+
+    trainer = Trainer(train_data=train_dataloader, dev_data=dev_dataloader, test_data=None, model=model, args=cfg, logger=logger, loss=loss, metrics=metrics, writer=writer)
+    trainer.train()
+
+    writer.close()
+
+
+if __name__ == "__main__":
+    main()
