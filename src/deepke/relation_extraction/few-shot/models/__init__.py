7e13236c1b7f78f88103faa83d8ed57cc8a166a1
TimelordRi <35120358+TimelordRi@users.noreply.github.com>
2021-09-29 22:37:36 +0800

    Delete src/deepke/relation_extraction/few-shot directory

--- src/deepke/relation_extraction/few-shot/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .bert import BertForSequenceClassification
-from .bert.modeling_bert import BertGetLabelWord, BertUseLabelWord, BertDecouple, BertForMaskedLM
-from .gpt2 import GPT2DoubleHeadsModel
-from .gpt2.modeling_gpt2 import GPT2UseLabelWord
-
-from .roberta import RobertaForSequenceClassification 
-from .roberta.modeling_roberta import RobertaUseLabelWord
