4a6e9ede88cca59f40c16b10983dfd866f2c5efe
tlk1997 <56509305+tlk1997@users.noreply.github.com>
2022-03-19 20:55:09 +0800

    Update dataset.py

--- src/deepke/relation_extraction/multimodal/modules/dataset.py
+++ src/deepke/relation_extraction/multimodal/modules/dataset.py
@@ -27,7 +27,7 @@ class MMREProcessor(object):
 
 
     def load_from_file(self, mode="train"):
-        load_file = self.data_path[mode]
+        load_file = os.path.join(self.args.cwd,self.data_path[mode])
         logger.info("Loading data from {}".format(load_file))
         with open(load_file, "r", encoding="utf-8") as f:
             lines = f.readlines()
@@ -46,20 +46,21 @@ class MMREProcessor(object):
         # 辅助图像
         aux_imgs = None
         # if not self.use_clip_vit:
-        aux_path = self.data_path[mode+"_auximgs"]
+        aux_path = os.path.join(self.args.cwd,self.data_path[mode+"_auximgs"])
         aux_imgs = torch.load(aux_path)
-        rcnn_imgs = torch.load(self.data_path[mode+'_img2crop'])
+        rcnn_imgs = torch.load(os.path.join(self.args.cwd,self.data_path[mode+'_img2crop']))
         return {'words':words, 'relations':relations, 'heads':heads, 'tails':tails, 'imgids': imgids, 'dataid': dataid, 'aux_imgs':aux_imgs, "rcnn_imgs":rcnn_imgs}
 
 
     def get_relation_dict(self):
-        with open(self.re_path, 'r', encoding="utf-8") as f:
+        
+        with open(os.path.join(self.args.cwd,self.re_path), 'r', encoding="utf-8") as f:
             line = f.readlines()[0]
             re_dict = json.loads(line)
         return re_dict
 
     def get_rel2id(self, train_path):
-        with open(self.re_path, 'r', encoding="utf-8") as f:
+        with open(os.path.join(self.args.cwd,self.re_path), 'r', encoding="utf-8") as f:
             line = f.readlines()[0]
             re_dict = json.loads(line)
         re2id = {key:[] for key in re_dict.keys()}
@@ -118,12 +119,12 @@ class MMREDataset(Dataset):
          # image process
         if self.img_path is not None:
             try:
-                img_path = os.path.join(self.img_path, imgid)
+                img_path = os.path.join(os.path.join(self.args.cwd,self.img_path), imgid)
                 img_path = img_path.replace('test', 'train')
                 image = Image.open(img_path).convert('RGB')
                 image = self.processor.clip_processor(images=image, return_tensors='pt')['pixel_values'].squeeze()
             except:
-                img_path = os.path.join(self.img_path, 'inf.png')
+                img_path = os.path.join(os.path.join(self.args.cwd,self.img_path), 'inf.png')
                 image = Image.open(img_path).convert('RGB')
                 image = self.processor.clip_processor(images=image, return_tensors='pt')['pixel_values'].squeeze()
             if self.aux_img_path is not None:
@@ -133,7 +134,8 @@ class MMREDataset(Dataset):
                 imgid = imgid.split(".")[0]
                 if item_id in self.data_dict['aux_imgs']:
                     aux_img_paths  = self.data_dict['aux_imgs'][item_id]
-                    aux_img_paths = [os.path.join(self.aux_img_path, path) for path in aux_img_paths]
+                    # print(aux_img_paths)
+                    aux_img_paths = [os.path.join(os.path.join(self.args.cwd,self.aux_img_path), path) for path in aux_img_paths]
                 # 大于3需要舍弃
                 for i in range(min(3, len(aux_img_paths))):
                     aux_img = Image.open(aux_img_paths[i]).convert('RGB')
@@ -152,7 +154,7 @@ class MMREDataset(Dataset):
                     rcnn_img_paths = []
                     if imgid in self.data_dict['rcnn_imgs']:
                         rcnn_img_paths = self.data_dict['rcnn_imgs'][imgid]
-                        rcnn_img_paths = [os.path.join(self.rcnn_img_path, path) for path in rcnn_img_paths]
+                        rcnn_img_paths = [os.path.join(os.path.join(self.args.cwd,self.rcnn_img_path), path) for path in rcnn_img_paths]
                      # 大于3需要舍弃
                     for i in range(min(3, len(rcnn_img_paths))):
                         rcnn_img = Image.open(rcnn_img_paths[i]).convert('RGB')
@@ -170,4 +172,5 @@ class MMREDataset(Dataset):
             
         return input_ids, token_type_ids, attention_mask, torch.tensor(re_label)
     
+
 
