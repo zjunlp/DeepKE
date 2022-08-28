b48891718fedbccb2708d83ee094a6cf8bbe4b4d
tlk-dsg <467460833@qq.com>
2022-08-28 23:57:18 +0800

    Update README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='deepke',  # 打包后的包文件名
-    version='2.0.5',    #版本号
+    version='2.1.0',    #版本号
     keywords=["pip", "RE","NER","AE"],    # 关键字
     description='DeepKE 是基于 Pytorch 的深度学习中文关系抽取处理套件。',  # 说明
     long_description="client",  #详细说明
@@ -29,7 +29,8 @@ setup(
         'wandb==0.12.7',
         "ujson",
         "huggingface_hub==0.2.1",
-        "pytorch-crf==0.7.2"
+        "pytorch-crf==0.7.2",
+        "pyld==2.0.3"
     ], 
     classifiers=[
         "Programming Language :: Python :: 3",
