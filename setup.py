339cd7d25c943316bbe9e7b254684bd231494ab9
tlk1997 <467460833@qq.com>
2022-12-28 21:45:10 +0800

    update requirement

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup, find_packages
 
-with open("README.md") as readme_file:
-    readme = readme_file.read()
 
 with open("requirements.txt") as requirements_file:
     requirements = requirements_file.read().splitlines()
     
 setup(
     name='deepke',  # 打包后的包文件名
-    version='2.1.1',    #版本号
+    version='2.1.2',    #版本号
     keywords=["pip", "RE","NER","AE"],    # 关键字
     description='DeepKE 是基于 Pytorch 的深度学习中文关系抽取处理套件。',  # 说明
-    long_description=readme,
-    long_description_content_type="text/markdown",
     license="MIT",  # 许可
     url='https://github.com/zjunlp/deepke',
     author='ZJUNLP',
