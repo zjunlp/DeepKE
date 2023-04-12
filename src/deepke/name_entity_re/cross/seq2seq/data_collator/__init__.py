9cf273a657e11971834846ba5b5f7124d6072d3e
Flow3rDown <1270494832@qq.com>
2023-04-12 21:37:55 +0800

    fix cross import bug

--- src/deepke/name_entity_re/cross/seq2seq/data_collator/__init__.py
+++ src/deepke/name_entity_re/cross/seq2seq/data_collator/__init__.py
@@ -2,16 +2,16 @@
 # -*- coding:utf-8 -*-
 
 
-from src.seq2seq.data_collator.meta_data_collator import (
+from .meta_data_collator import (
     DataCollatorForMetaSeq2Seq,
     DynamicSSIGenerator,
 )
 
-from src.seq2seq.data_collator.t5mlm_data_collator import (
+from .t5mlm_data_collator import (
     DataCollatorForT5MLM,
 )
 
-from src.seq2seq.data_collator.hybird_data_collator import (
+from .hybird_data_collator import (
     HybirdDataCollator,
 )
 
