4e4620053ea44812ec50a8ed8101b387e31b7c45
Xin Xu <xxucs@zju.edu.cn>
2023-05-12 20:02:38 +0800

    Update InferBert.py

--- src/deepke/name_entity_re/standard/models/InferBert.py
+++ src/deepke/name_entity_re/standard/models/InferBert.py
@@ -22,7 +22,7 @@ class BertNer(BertForTokenClassification):
         attention_mask=None, 
         valid_ids=None
     ):
-        sequence_output = self.bert(input_ids, token_type_ids, attention_mask, head_mask=None)[0]
+        sequence_output = self.bert(input_ids=input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask, head_mask=None)[0]
         batch_size,max_len,feat_dim = sequence_output.shape
         valid_output = torch.zeros(batch_size,max_len,feat_dim,dtype=torch.float32,device='cuda' if torch.cuda.is_available() else 'cpu')
         for i in range(batch_size):
