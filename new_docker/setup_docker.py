debf81dcc9578c8d269a4513c288b602d0617b23
hunxuewangzi <80691681+hunxuewangzi@users.noreply.github.com>
2023-08-03 21:07:45 +0800

    Add files via upload

---
+++ new_docker/setup_docker.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+    
+setup(
+    name='deepke',  # 打包后的包文件名
+    version='2.2.1',    #版本号
+    keywords=["pip", "RE","NER","AE"],    # 关键字
+    description='DeepKE is a knowledge extraction toolkit for knowledge graph construction supporting low-resource, document-level and multimodal scenarios for entity, relation and attribute extraction.',  # 说明
+    license="MIT",  # 许可
+    url='https://github.com/zjunlp/deepke',
+    author='ZJUNLP',
+    author_email='zhangningyu@zju.edu.cn',
+    include_package_data=True,
+    platforms="any",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
