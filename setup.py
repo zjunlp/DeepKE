18b0118497948c6fd0265f977b8be493a96ece80
Shuofei Qiao <920722741@qq.com>
2023-08-05 20:52:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as requirements_file:
     
 setup(
     name='deepke',  # 打包后的包文件名
-    version='2.2.1',    #版本号
+    version='2.2.6',    #版本号
     keywords=["pip", "RE","NER","AE"],    # 关键字
     description='DeepKE is a knowledge extraction toolkit for knowledge graph construction supporting low-resource, document-level and multimodal scenarios for entity, relation and attribute extraction.',  # 说明
     license="MIT",  # 许可
