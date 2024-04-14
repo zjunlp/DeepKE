50ffc68d7a06fab6e4c7871ed269491470680836
Xin Xu <xxucs@zju.edu.cn>
2024-04-14 12:43:13 +0800

    Update run.py

--- example/re/document/run.py
+++ example/re/document/run.py
@@ -14,6 +14,7 @@ from deepke.relation_extraction.document import *
 
 import wandb
 
+
 def train(args, model, train_features, dev_features, test_features):
     def logging(s, print_=True, log_=True):
         if print_:
@@ -87,10 +88,11 @@ def train(args, model, train_features, dev_features, test_features):
                                 epoch, global_step, elapsed / 60, scheduler.get_last_lr(), cur_loss * 1000))
                         total_loss = 0
                         start_time = time.time()
-
-                        wandb.log({
-                            "train_loss":cur_loss
-                        })
+                        
+                        if args.use_wandb:
+                            wandb.log({
+                                "train_loss":cur_loss
+                            })
 
                 if (step + 1) == len(train_dataloader) - 1 or (args.evaluation_steps > 0 and num_steps % args.evaluation_steps == 0 and step % args.gradient_accumulation_steps == 0):
                 # if step ==0:
@@ -101,20 +103,21 @@ def train(args, model, train_features, dev_features, test_features):
                     logging(
                         '| epoch {:3d} | time: {:5.2f}s | dev_result:{}'.format(epoch, time.time() - eval_start_time,
                                                                                 dev_output))
-
-                    wandb.log({
-                            "dev_result":dev_output
-                    })
+                    if args.use_wandb:
+                        wandb.log({
+                                "dev_result":dev_output
+                        })
 
                     logging('-' * 89)
                     if dev_score > best_score:
                         best_score = dev_score
                         logging(
                             '| epoch {:3d} | best_f1:{}'.format(epoch, best_score))
-
-                        wandb.log({
-                            "best_f1":best_score
-                        })
+                        
+                        if args.use_wandb:
+                            wandb.log({
+                                "best_f1":best_score
+                            })
 
                         if args.save_path != "":
                             torch.save({
@@ -191,10 +194,12 @@ def evaluate(args, model, features, tag="dev"):
     return best_f1, output
 
 
-wandb.init(project="DeepKE_RE_Document")
-wandb.watch_called = False
 @hydra.main(config_path="conf/config.yaml")
 def main(cfg):
+    if cfg.use_wandb:
+        wandb.init(project="DeepKE_RE_Document")
+        wandb.watch_called = False
+    
     cwd = get_original_cwd()
     os.chdir(cwd)
 
@@ -226,7 +231,8 @@ def main(cfg):
         from_tf=bool(".ckpt" in cfg.model_name_or_path),
         config=config,
     )
-    wandb.watch(model, log="all")
+    if cfg.use_wandb:
+        wandb.watch(model, log="all")
 
 
     config.cls_token_id = tokenizer.cls_token_id
