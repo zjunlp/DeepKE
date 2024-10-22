004e608579d3caf4033d7a84477f4c9db698a531
guihonghao <53286894+guihonghao@users.noreply.github.com>
2024-10-22 16:41:53 +0800

    Update training_args.py

--- example/llm/InstructKGC/src/args/training_args.py
+++ example/llm/InstructKGC/src/args/training_args.py
@@ -13,6 +13,6 @@ class TrainingArguments(Seq2SeqTrainingArguments):
 
     gradient_checkpointing: bool = field(default=False, metadata={"help": 'Use gradient checkpointing. You want to use this.'})
     remove_unused_columns: bool = field(default=False, metadata={"help": 'Removed unused columns. Needed to make this codebase work.'})
-    group_by_length: bool = field(default=True, metadata={"help": 'Group sequences into batches with same length. Saves memory and speeds up training considerably.'})
+    group_by_length: bool = field(default=False, metadata={"help": 'Group sequences into batches with same length. Saves memory and speeds up training considerably.'})
     report_to: str = field(default='none', metadata={"help": "To use wandb or something else for reporting."})
     logging_steps: float = field(default=2, metadata={"help": "Log every X updates steps. Should be an integer or a float in range `[0,1)`. If smaller than 1, will be interpreted as ratio of total training steps."})
